ed05db12f060f1958ca517113cdfb1486ceef379
Jewynah <jhjaronay@gmail.com>
2024-05-27 07:25:33 +0800

    feature/calculator-feature
    Changes:
    Added delay for hand detection and gesture detection
    Added conditionals for making expression
    Added a way to solve expression

--- prog/app.py
+++ prog/app.py
@@ -4,10 +4,19 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import json
 
-detected_digits = []
+detected_digits = ""
 last_detected_digit = None
+cooldown = 0
+state = "number"
+expression = ""
+result = ""
 
-model = load_model('hand_gesture_model.h5')
+hand_cooldown = 120
+
+model = load_model('C:\\Users\\Jewy\\Documents\\Mathematical-Expressions-Input-Using-Hand-Gestures\\prog\\hand_gesture_model.h5')
+
+cv2.namedWindow("Detected Digits", cv2.WINDOW_NORMAL)
+
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
@@ -35,32 +44,69 @@ while True:
     # convert frame to rgb
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    keypoints = extract_keypoints(frame)
-    if keypoints is not None:
-        keypoints = keypoints.reshape(1, -1)
-        prediction = model.predict(keypoints)
-        predicted_class = np.argmax(prediction)
+    if hand_cooldown == 0:
+        keypoints = extract_keypoints(frame)
+        if keypoints is not None and cooldown == 0:
+            keypoints = keypoints.reshape(1, -1)
+            prediction = model.predict(keypoints)
+            predicted_class = np.argmax(prediction)
 
-        if predicted_class != last_detected_digit:
-            detected_digits.append(int(predicted_class))  # Append the detected digit to the list
-            last_detected_digit = predicted_class
+            if predicted_class != last_detected_digit:
+                if predicted_class == 1:
+                    detected_digits += str ("+")
+                    #detected_digits = detected_digits[:-1]
+                    last_detected_digit = predicted_class
+                    cooldown = 120
+                elif predicted_class == 2:
+                    detected_digits += str ("-")
+                    #detected_digits = detected_digits[:-1]
+                    last_detected_digit = predicted_class
+                    cooldown = 120
+                elif predicted_class == 3:
+                    detected_digits += str ("*")
+                    #detected_digits = detected_digits[:-1]
+                    last_detected_digit = predicted_class
+                    cooldown = 120
+                elif predicted_class == 4:
+                    detected_digits += str ("/")
+                    #detected_digits = detected_digits[:-1]
+                    last_detected_digit = predicted_class
+                    cooldown = 120
+                #elif detected_digits and detected_digits[-1] == '0':
+                elif predicted_class == 0:
+                    result = eval(detected_digits[:-1]) 
+                    detected_digits += str ("=") 
+                    detected_digits += str (result)
+                else:
+                    detected_digits += str (predicted_class)
+            cv2.putText(frame, f'Prediction: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            hand_cooldown = 120
+    else:
+        hand_cooldown -= 1
+    
+    frame_height = frame.shape[0]
+    text_image = np.zeros((50, 500, 3), dtype=np.uint8)
+    cv2.putText(text_image, f'{detected_digits}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow("Detected Digits", text_image)
 
-        cv2.putText(frame, f'Prediction: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        frame_height = frame.shape[0]
-        detected_digits_str = ''.join(map(str, detected_digits))
-        cv2.putText(frame, f'Input: {detected_digits_str}', (10, frame_height - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    if cooldown > 0:
+        cooldown -= 1
 
     # find hands
     results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        
+    
 
     # display result
     cv2.imshow('Hand Gesture Recognition', frame)
-
-    # q for exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        detected_digits = ""
+        clear_image = np.zeros((50, 500, 3), dtype=np.uint8)
+        cv2.imshow("Detected Digits", clear_image)
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # release webcam
@@ -71,4 +117,4 @@ cv2.destroyAllWindows()
 with open('detected_digits.json', 'w') as f:
     json.dump(detected_digits, f)
 
-print("Detected digits:", detected_digits)
+

438e3248772f24b3d6662e33acc92557ed316e49
Jewynah <jhjaronay@gmail.com>
2024-05-27 20:06:38 +0800

    version1.1

--- prog/app.py
+++ prog/app.py
@@ -5,12 +5,11 @@ from tensorflow.keras.models import load_model
 import json
 
 detected_digits = ""
-last_detected_digit = None
-cooldown = 0
 state = "number"
 expression = ""
 result = ""
 hand_cooldown = 120
+isdone = False
 
 model = load_model('C:\\Users\\Jewy\\Documents\\Mathematical-Expressions-Input-Using-Hand-Gestures\\prog\\hand_gesture_model.h5')
 
@@ -44,60 +43,55 @@ while True:
     
     if hand_cooldown == 0:
         keypoints = extract_keypoints(frame)
-        if keypoints is not None and cooldown == 0:
+        if keypoints is not None and isdone == False:
             keypoints = keypoints.reshape(1, -1)
             prediction = model.predict(keypoints)
             predicted_class = np.argmax(prediction)
 
-            if predicted_class != last_detected_digit:
-                if predicted_class == 10:
-                    detected_digits += str ("+")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                elif predicted_class == 11:
-                    detected_digits += str ("-")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                elif predicted_class == 12:
-                    detected_digits += str ("*")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                elif predicted_class == 13:
-                    detected_digits += str ("/")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                elif predicted_class == 14:
-                    detected_digits += str ("(")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                elif predicted_class == 15:
-                    detected_digits += str (")")
-                    #detected_digits = detected_digits[:-1]
-                    last_detected_digit = predicted_class
-                    cooldown = 120
-                #elif detected_digits and detected_digits[-1] == '0':
-                elif predicted_class == 16:
-                    result = eval(detected_digits) 
-                    detected_digits += str ("=") 
-                    detected_digits += str (result)
-                else:
-                    detected_digits += str (predicted_class)
-            cv2.putText(frame, f'Prediction: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            hand_cooldown = 120
+          
+            if predicted_class == 10:
+                detected_digits += str ("+")
+                #detected_digits = detected_digits[:-1]
+                
+                
+            elif predicted_class == 11:
+                detected_digits += str ("-")
+                #detected_digits = detected_digits[:-1]
+                
+                
+            elif predicted_class == 12:
+                detected_digits += str ("*")
+                #detected_digits = detected_digits[:-1]
+                
+                
+            elif predicted_class == 13:
+                detected_digits += str ("/")
+                #detected_digits = detected_digits[:-1]
+                
+                
+            elif predicted_class == 14:
+                detected_digits += str ("(")
+                #detected_digits = detected_digits[:-1]
+
+                
+            elif predicted_class == 15:
+                detected_digits += str (")")
+
+            elif predicted_class == 16:
+                result = eval(detected_digits) 
+                detected_digits += str ("=") 
+                detected_digits += str (result)
+                isdone = True
+            else:
+                detected_digits += str (predicted_class)
+        cv2.putText(frame, f'Prediction: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        hand_cooldown = 120
     else:
         hand_cooldown -= 1
         frame_height = frame.shape[0]
     text_image = np.zeros((50, 500, 3), dtype=np.uint8)
     cv2.putText(text_image, f'{detected_digits}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     cv2.imshow("Detected Digits", text_image)
-
-    if cooldown > 0:
-        cooldown -= 1
 
     # find hands
     results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
@@ -115,6 +109,12 @@ while True:
         cv2.imshow("Detected Digits", clear_image)
     elif cv2.waitKey(1) & 0xFF == ord('x'):
         detected_digits = detected_digits[:-1]
+        
+    elif cv2.waitKey(1) & 0xFF == ord('v'):
+        isdone = False
+        detected_digits = ""
+        clear_image = np.zeros((50, 500, 3), dtype=np.uint8)
+        cv2.imshow("Detected Digits", clear_image)
     elif cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
